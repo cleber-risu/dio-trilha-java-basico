bda0d44d00f11794ee1db3056018e78fae81b495
cleber <cleber.risu@outlook.com>
2025-03-27 20:59:56 -0300

    tipos de dados

--- aprendendo-sintaxe-java/src/edu/cleber/tiposvariaveis/TiposVariaveis.java
+++ aprendendo-sintaxe-java/src/edu/cleber/tiposvariaveis/TiposVariaveis.java
@@ -2,6 +2,19 @@ package edu.cleber.tiposvariaveis;
 
 public class TiposVariaveis {
   public static void main(String[] args) {
-    System.out.println("hello");
+    
+    // TIPOS DE DADOS
+
+    // (1 byte) -128 a 127
+    byte idade = 123;
+    
+    // (2 byte) -32768 a 32767 
+    short ano = 2025;
+    
+    // (4 bytes) -2.147.483.648 a 2.147.483.647
+    int cep = 21070333;
+    
+    // (8 bytes) -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+    long cpf = 98765432109L;  
   }
 }
