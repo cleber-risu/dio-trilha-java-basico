df8422724c80dc37fce5e60d3a0ab014dd158a11
cleber <cleber.risu@outlook.com>
2025-03-28 19:28:15 -0300

    java doc

---
+++ aprendendo-sintaxe-java/src/edu/cleber/javadoc/Calculadora.java
@@ -0,0 +1,26 @@
+package edu.cleber.javadoc;
+
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Node: </b> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Cleber Risu
+ * @version 1.0
+ * @since 28/03/2025
+ */
+public class Calculadora {
+
+  /**
+   * Este método é utilizado para somar dois números inteiros
+   * 
+   * @param numeroUm
+   * @param numeroDois
+   * @return int o resultado deste método é soma de dois números inteiros
+   */
+  public int somar(int numeroUm, int numeroDois) {
+    return numeroUm + numeroDois;
+  }
+}
---
+++ aprendendo-sintaxe-java/src/edu/cleber/javadoc/Comentarios.java
@@ -0,0 +1,17 @@
+package edu.cleber.javadoc;
+
+public class Comentarios {
+  public static void main(String[] args) {
+    // sou um comentario de uma linha
+
+    /*
+     * sou um documentario de multiplas linhas
+     */
+  }
+
+  /**
+   * sou um comentario de documentação
+   */
+  public void metodo() {
+  }
+}
