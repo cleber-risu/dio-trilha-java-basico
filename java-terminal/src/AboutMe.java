943168d3346ddd7841203913a3f8ae20a0faa232
cleber <cleber.risu@outlook.com>
2025-03-28 20:08:25 -0300

    java terminal

---
+++ java-terminal/src/AboutMe.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+  public static void main(String[] args) {
+    // os argumentos começam com indice 0
+    // String nome = args[0] ;
+    // String sobrenome = args[1];
+    // int idade = Integer.valueOf(args[2]); // vamos falar sobre Wrappers
+    // double altura = Double.valueOf(args[3]);
+
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Digite o seu nome: ");
+    String nome = scanner.next();
+
+    System.out.println("Digite o seu sobrenome: ");
+    String sobrenome = scanner.next();
+
+    System.out.println("Digite sua idade: ");
+    int idade = scanner.nextInt();
+
+    System.out.println("Digite a sua altura: ");
+    double altura = scanner.nextDouble();
+
+    System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+    System.out.println("Tenho " + idade + " anos ");
+    System.out.println("Minha altura é " + altura + "cm ");
+  }
+}
