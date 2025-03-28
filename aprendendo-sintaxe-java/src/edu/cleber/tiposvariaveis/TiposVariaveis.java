8c8e077e5150cdca0661d5017a60b0496b7e2077
cleber <cleber.risu@outlook.com>
2025-03-27 21:06:31 -0300

    constantes

--- aprendendo-sintaxe-java/src/edu/cleber/tiposvariaveis/TiposVariaveis.java
+++ aprendendo-sintaxe-java/src/edu/cleber/tiposvariaveis/TiposVariaveis.java
@@ -22,5 +22,12 @@ public class TiposVariaveis {
     int idade; //Tipo "int", nome "idade", com nenhum valor atribuído. 
     int anoFabricacao = 2021; //tipo "int", nome "anoFabricacao", com valor 2021.
     double salarioMinimo = 2.500; //tipo "double", nome "salarioMinimo", valor 2.500.
+
+
+    // CONTANTES
+
+    int numero = 5;
+    numero = 10;
+    final double VALOR_DE_PI = 3.14; // constantes tem que iniciar com final e seu nome em caixa alta
   }
 }
