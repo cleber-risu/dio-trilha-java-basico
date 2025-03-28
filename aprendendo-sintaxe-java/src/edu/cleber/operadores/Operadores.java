bab9aa446f50281418cc59e800213691732d71ae
cleber <cleber.risu@outlook.com>
2025-03-28 14:28:50 -0300

    Operadores Aritmeticos

---
+++ aprendendo-sintaxe-java/src/edu/cleber/operadores/Operadores.java
@@ -0,0 +1,15 @@
+package edu.cleber.operadores;
+
+public class Operadores {
+  public static void main(String[] args) {
+    System.out.println("Operadores Aritmeticos");
+    
+    double soma = 10.5 + 15.7;
+    int subtracao = 113 - 25;
+    int multiplicacao = 20 * 7;
+    int divisao = 15 / 3;
+    int modulo = 18 % 3; // resto
+    double resultado = (10 * 7) + (20 / 4);
+
+  }
+}
--- aprendendo-sintaxe-java/src/edu/cleber/tiposvariaveis/TiposVariaveis.java
+++ aprendendo-sintaxe-java/src/edu/cleber/tiposvariaveis/TiposVariaveis.java
@@ -19,7 +19,7 @@ public class TiposVariaveis {
     
     // DECLARACAO DE VARIAVEIS
 
-    int idade; //Tipo "int", nome "idade", com nenhum valor atribuído. 
+    int idade2; //Tipo "int", nome "idade", com nenhum valor atribuído. 
     int anoFabricacao = 2021; //tipo "int", nome "anoFabricacao", com valor 2021.
     double salarioMinimo = 2.500; //tipo "double", nome "salarioMinimo", valor 2.500.
 
